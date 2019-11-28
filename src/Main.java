ef6965c2e2aca760159603fc03b221ea6adf522e
solicat <giant3380@gmail.com>
2019-11-28 15:10:29 +0900

    Add some attibute

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,39 @@
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
 
 import dm.Account_data;
 import hci.login.login;
+import pd.management.Account;
+import pd.management.Administrator;
+import pd.student_data.Student;
 
 public class Main {
 
 	public static void main(String[] args) {
+	
+		ArrayList<Account> iaccount = new ArrayList<Account>();
 
-		Scanner kb = new Scanner(System.in);
-		/*
-		ArrayList<Account> account = new ArrayList<Account>();
+		iaccount.add(new Account("0000000000", "qwer", new Student()));
+		iaccount.add(new Account("1111111111", "asdf", new Student()));
+		iaccount.add(new Account("2222222222", "zxcv", new Administrator()));
 
-		account.add(new Account("0000000000", "qwer", new Student()));
-		account.add(new Account("1111111111", "asdf", new Student()));
-		account.add(new Account("2222222222", "zxcv", new Administrator()));
-
-		for (Account iter : account)
+		for (Account iter : iaccount)
 			System.out.println(iter.getID() + " " + iter.getPW() + " " + iter.getUser().getClass());
 		try {
 			ObjectOutputStream ostream = new ObjectOutputStream(new FileOutputStream("Account.dat"));
-			ostream.writeObject(account);
+			ostream.writeObject(iaccount);
 
 			ostream.close();
 		} catch (IOException e) {
 			System.out.println("Problem with file output.");
 		}
-*/
+
 		Account_data account = new Account_data();
 		account.openAccount();
 		
--- src/pd/student_data/Career.java
+++ src/pd/student_data/Career.java
@@ -1,10 +1,16 @@
 package pd.student_data;
 
+import java.util.ArrayList;
+
 public class Career {
 	
 	private int total_credit;
 	private int count_counseling;
-	
+	private ArrayList<Exchange> exchange;
+	private ArrayList<Intern> intern;
+	private ArrayList<Eng_Grade> eng_grade;
+	private ArrayList<Subject> subject;	
+
 	public Career(int total_credit, int count_counseling) {
 		super();
 		this.total_credit = total_credit;
@@ -13,8 +19,16 @@ public class Career {
 	
 	public int getTotal_credit() {return total_credit;}
 	public int getCount_counseling() {return count_counseling;}
+	public ArrayList<Exchange> getExchange() {return exchange;}
+	public ArrayList<Intern> getIntern() {return intern;}
+	public ArrayList<Eng_Grade> getEng_grade() {return eng_grade;}
+	public ArrayList<Subject> getSubject() {return subject;}
 	public void setTotal_credit(int total_credit) {this.total_credit = total_credit;}
 	public void setCount_counseling(int count_counseling) {this.count_counseling = count_counseling;}
+	public void setExchange(ArrayList<Exchange> exchange) {this.exchange = exchange;}
+	public void setIntern(ArrayList<Intern> intern) {this.intern = intern;}
+	public void setEng_grade(ArrayList<Eng_Grade> eng_grade) {this.eng_grade = eng_grade;}
+	public void setSubject(ArrayList<Subject> subject) {this.subject = subject;}
 	
 	public void check_requirement()
 	{
