cd88a9ca5fd4d7587880e79ce740614f35548ae0
solicat <giant3380@gmail.com>
2019-12-05 17:32:41 +0900

    Relocate code in main

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,9 @@ import pd.student_data.*;
 public class Main {
 
 	public static void main(String[] args) {
-	
+
 		ArrayList<Account> iaccount = new ArrayList<Account>();
-		
+
 		iaccount.add(new Account("0000000000", "qwer", new Student()));
 		iaccount.add(new Account("1111111111", "asdf", new Student()));
 		iaccount.add(new Account("2222222222", "zxcv", new Administrator()));
@@ -31,44 +31,48 @@ public class Main {
 			System.out.println("Problem with file output.");
 		}
 
-		Account_data account = new Account_data();
-		account.openAccount();
-		
-		Notice_data data = new Notice_data();
-		data.openNotice();
-		
-		login loginGui = new login();
-		loginGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		loginGui.setVisible(true);		
-
-	
 		ArrayList<Subject> e_sub = new ArrayList<Subject>();
-		e_sub.add(new Subject("Success","code","name","prof"));
-		
+		e_sub.add(new Subject("Success", "code", "name", "prof"));
+
 		ArrayList<Requirement> req = new ArrayList<Requirement>();
-		req.add(new Requirement(new Language_score("TOEIC",55.5),new Foriegn_exchange("USA","aa",30),	new Intern("bbb",5,5),e_sub,1,2,3,4));
-		for(Requirement i:req) {
+		req.add(new Requirement(new Language_score("TOEIC", 55.5), new Foriegn_exchange("USA", "aa", 30),
+				new Intern("bbb", 5, 5), e_sub, 1, 2, 3, 4));
+		for (Requirement i : req) {
 			System.out.println(i.getLanguage_score().getType() + " " + i.getLanguage_score().getScore());
-			System.out.println(i.getForiegn_exchange().getCountry_name() + " " + i.getForiegn_exchange().getUniv_name() + " " + i.getForiegn_exchange().getCredit());
-			System.out.println(i.getIntern().getCompany_name() + " " + i.getIntern().getPeriod() + " " + i.getIntern().getCredit());
-			for(Subject j:i.getEssential_subject())
-				System.out.println("'" + j.getCourse_name() + " " + j.getCode() + " " + j.getCourse_name() + " " + j.getProf_name() + "'");
+			System.out.println(i.getForiegn_exchange().getCountry_name() + " " + i.getForiegn_exchange().getUniv_name()
+					+ " " + i.getForiegn_exchange().getCredit());
+			System.out.println(i.getIntern().getCompany_name() + " " + i.getIntern().getPeriod() + " "
+					+ i.getIntern().getCredit());
+			for (Subject j : i.getEssential_subject())
+				System.out.println("'" + j.getCourse_name() + " " + j.getCode() + " " + j.getCourse_name() + " "
+						+ j.getProf_name() + "'");
 			System.out.println(i.getConsult_cnt() + " " + i.getMajor() + " " + i.getTrack() + " " + i.getIn_year());
-		
+
 		}
 		try {
-			ObjectOutputStream ostream = new ObjectOutputStream(new FileOutputStream("Account.dat"));
-			ostream.writeObject(req);			
-			
+			ObjectOutputStream ostream = new ObjectOutputStream(new FileOutputStream("Requirement.dat"));
+			ostream.writeObject(req);
+
 			ostream.close();
 		} catch (IOException e) {
 			System.out.println("Problem with file output.");
 		}
+
+		
+		Account_data account = new Account_data();
+		account.openAccount();
+
+		Notice_data data = new Notice_data();
+		data.openNotice();
 		
 		Requirement_data requirement = new Requirement_data();
 		requirement.openRequirement();
-//		requirement.createRequirement(req);
+
 		
 		
+		login loginGui = new login();
+		loginGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		loginGui.setVisible(true);
+
 	}
 }
